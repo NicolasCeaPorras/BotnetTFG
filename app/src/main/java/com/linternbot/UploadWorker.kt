17c5c4b3a80a55b1a490b5c6ae7bc969a892e6b6
NicolasCeaPorras <nicolasceaporras@gmail.com>
2022-03-15 17:57:50 +0100

    Ahora en firebase se van haciendo entradas con la hora

--- app/src/main/java/com/linternbot/UploadWorker.kt
+++ app/src/main/java/com/linternbot/UploadWorker.kt
@@ -4,13 +4,27 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import kotlinx.coroutines.delay
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
 import java.util.*
 
 class UploadWorker(appContext: Context, workerParams: WorkerParameters): Worker(appContext, workerParams) {
     override fun doWork(): Result {
 
+        val c = Calendar.getInstance()
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val strDate: String = sdf.format(c.time)
         // Do the work here--in this case, upload the images.
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "Bot ID" to "Movil Nico",
+            "Hora" to strDate
+        )
+
+        // Add a new document with a generated ID
+        db.collection("ImAlive").document(strDate).set(user)
+
         Log.d("TAG2","Se ejecuta la tarea programada")
 
         // Indicate whether the work finished successfully with the Result
