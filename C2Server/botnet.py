961fd6b2e07b4302d7dfc18520670e01c9901d2b
NicolasCeaPorras <45874015+NicolasCeaPorras@users.noreply.github.com>
2022-03-15 14:10:06 -0400

    C2 Server

---
+++ C2Server/botnet.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials, firestore# initialize sdk
+from datetime import datetime
+
+cred = credentials.Certificate("tfg-nicolas-cea-firebase-adminsdk-bq588-0a7759e31d.json")
+
+firebase_admin.initialize_app(cred)# initialize firestore instance
+
+firestore_db = firestore.client()# add data
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+# Escribe una orden para la botnet
+firestore_db.collection(u'ordenes').document('BotMaster').set({'Primitiva': 'Prueba', 'Hora': dt_string})
